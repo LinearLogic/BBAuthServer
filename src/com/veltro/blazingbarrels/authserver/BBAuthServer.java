1b909a89310fdf66e74bc0141cb65f1dac0a78bf
Jared Heath <silkrawnstudios@gmail.com>
2013-02-12 19:55:45 -0800

    [0.0.1] Initial commit - includes main class,
    README, and .git files

---
+++ src/com/veltro/blazingbarrels/authserver/BBAuthServer.java
@@ -0,0 +1,14 @@
+package com.veltro.blazingbarrels.authserver;
+
+/**
+ * Program entry point and command handler
+ * 
+ * @author LinearLogic
+ * @version 0.0.1
+ */
+public class BBAuthServer {
+
+	public static void main(String[] args) {
+		
+	}
+}
